e3398a641355b05c5314becb02ea52e0b573b93e
Bran62914 <brandon.jansky@trinitaschristian.org>
2025-04-15 19:06:41 +0000

    added java script for secret code and extra time stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,38 @@
+//these are all const because we expect no changes to their value
 const h1 = document.getElementById("title");
-const el = document.querySelector("#currenttime");
 const h2 = document.querySelector("h2");
-const username = prompt("What is your name traveler?");
+const el = document.querySelector("#currenttime");
+const username = prompt("What is your name boy?");
 
-if ("f22 are awsome" == prompt("What is secret message?") ) {
+
+
+//We are going to change this value at least once more
+let timevalue;
+
+//Can the use see our secret content
+if ("gopackgo" == prompt("What is secret message?") ) {
     h2.hidden = false;
 } 
+let tries = 10;
+let userAnswer = prompt("What is the secret message");
+
+while ( userAnswer != "gopackgo" && tries > 0 ) {
+    alert("your answer is invaild. Try again! You only have " + tries + " attempts remaining");
+    userAnswer = prompt("What is the secret message");
+    tries = tries - 1;
+}
 
 
-
+//updates our clock every second
 setInterval( setTime, 1000);
-
+//greets the user by the name provided
 h1.textContent = "Hello," + username;
 
 function setTime() {
-    el.textContent = new Date().toLocaleTimeString();
+    timevalue = new Date().toLocaleTimeString();
+    renderTime();
+}
+function renderTime() {
+    el.textContent = timevalue;
 }
     
