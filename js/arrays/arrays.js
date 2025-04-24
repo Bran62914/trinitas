090dcdef86bc6e3dff4845aa36be17c46b3db819
Bran62914 <brandon.jansky@trinitaschristian.org>
2025-04-24 19:05:59 +0000

    added more to the js page loke ages and initials and future ages

--- js/arrays/arrays.js
+++ js/arrays/arrays.js
@@ -5,8 +5,8 @@ const students = [
 [ "Brandon" , "Jansky", 14 ],
 [ "Jackson" , "Cowart", 15 ],
 ];
+//We crete an array of students that contains varibles and arry literals
 
-const jon = [ "Jon", "Sampson", 15 ];
 
 function printStudentList () {
     for( const student of students) {
@@ -17,4 +17,22 @@ function printStudentList () {
         console.log( firstName + " will be " + futureAge + " in 10 years ");
     }
 }
-printStudentList();
+//lets find all students that are 15+
+//The arrow pointing at the return is a funtion thing(this is more mordern)
+// we can get rid of the return by getting rid of the return and the curly braces
+//it still work we can even give it  a differant name because we can use any name we want like this (s) => s[2] >= 15
+const fifteenPlus = students.filter((student) =>  student[2] >= 15 );
+// 0 acsses the first name in students 
+const studentInitials = fifteenPlus.map((student) => student[0][0] + student[1][0] );
+const futureAges = fifteenPlus.map((student) => 10 + student[2] );
+
+// for( const student of students) {
+//     //Zero-based index (zero is the first index/location in  list, always)
+//     const age = student[2];
+//     if ( age >= 15 ) {
+//         fifteenPlus.push(student);
+//     }
+// }
+
+console.log(fifteenPlus, futureAges, studentInitials)
+// printStudentList();
